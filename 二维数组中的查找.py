372841b029d20d642a81f7bff062c26c473e2caf
ZhaoKaidii <49524575+ZhaoKaidii@users.noreply.github.com>
2021-08-13 05:04:14 +0800

    matrix

---
+++ 二维数组中的查找.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findNumberIn2DArray(self, matrix, target):
+        c=False
+        if len(matrix)==0 or len(matrix[0])==0:
+            c=False
+        else:
+            import numpy as np 
+            m,n=np.shape(matrix)
+            for i in range(m):
+                if target<=matrix[i][n-1]:
+                    for j in range(n):
+                        c= c or(target==matrix[i][j])
+        return c
