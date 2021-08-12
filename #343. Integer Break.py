cc1333a739aa8b5d1bc070503645b8b2539d72c9
ZhaoKaidii <49524575+ZhaoKaidii@users.noreply.github.com>
2021-08-13 06:23:57 +0800

    dynamical programming

---
+++ #343. Integer Break.py
@@ -0,0 +1,9 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n<4:
+            return int(n/2)*(n-int(n/2))
+        f=0
+        for i in range(2,int(n/2)+1):
+            c=max((n%i)*(i**(n//i)),(i+n%i)*(i**((n//i)-1)))
+            f=max(c,f)
+        return f
