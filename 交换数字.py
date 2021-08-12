1250b418f0ec5e7b8d40377484115d390ffa2e08
ZhaoKaidii <49524575+ZhaoKaidii@users.noreply.github.com>
2021-08-13 03:59:24 +0800

    math

---
+++ 交换数字.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def swapNumbers(self, numbers):
+        numbers[0]=numbers[1]+numbers[0]
+        numbers[1]=numbers[0]-numbers[1]
+        numbers[0]=numbers[0]-numbers[1]
+        return numbers
