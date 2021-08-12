f23974d0c402ae999d2c9b18f78eb17768a28dc1
ZhaoKaidii <49524575+ZhaoKaidii@users.noreply.github.com>
2021-08-13 06:30:20 +0800

    dynamical programming

---
+++ #322. Coin Change.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        dp=[float('inf')]*(amount+1)
+        dp[0]=0
+        for coin in coins:
+            for i in range(coin,amount+1):
+                dp[i]=min(dp[i],dp[i-coin]+1)
+        return dp[-1] if dp[-1]!=float('inf') else -1
