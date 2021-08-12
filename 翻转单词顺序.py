539fef1ca3bd89d434bc25a7d537b7d280c73cb4
ZhaoKaidii <49524575+ZhaoKaidii@users.noreply.github.com>
2021-08-13 06:54:22 +0800

    string

---
+++ 翻转单词顺序.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def reverseWords(self, s):
+        ss=[]
+        s=s+" "
+        c=d=""
+        for i in s:
+            if i==" ":
+                if c==" " or c=="":
+                    continue
+                else: 
+                    ss.append(c)
+                    c=""
+            else:
+                c=c+i      
+        for j in range(len(ss)-1,-1,-1):
+            if j==0:
+                d=d+ss[j]
+            else:
+                d=d+ss[j]+" "
+        return d
