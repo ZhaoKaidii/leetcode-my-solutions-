8a8e555012591c5d651562669c59e2cbf14aa4fe
ZhaoKaidii <49524575+ZhaoKaidii@users.noreply.github.com>
2021-08-13 09:42:41 +0800

    prefix sum/ Hash table

---
+++ #525. Contiguous Array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findMaxLength(self, nums):
+        dic={0:-1}  
+        ans,count=0,0
+        for i,num in enumerate(nums):
+            if num==1:
+                count+=1
+            else:
+                count-=1
+            if count in dic:
+                ans=max(ans,i-dic[count])
+            else:
+                dic[count]=i
+        return ans
