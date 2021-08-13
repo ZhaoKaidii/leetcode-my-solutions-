fe1d972cb1f2bb3266526b636bf66362c3c9bec2
ZhaoKaidii <49524575+ZhaoKaidii@users.noreply.github.com>
2021-08-13 08:47:13 +0800

    greedy

---
+++ #1323. Maximum 69 Number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maximum69Number (self, num):
+        s=[c for c in str(num)]
+        for i in range(len(s)):
+            if s[i]=="6":
+                s[i]="9"
+                break
+        return int("".join(s))
